694332d7044e1a59a01dd3e209940b860b698f76
Vidya Rangasayee <vidya@hunt.com>
2016-09-20 11:57:12 -0700

    Barebones fragment demo (Not yet hooked up)

--- FragmentDemo/app/src/main/java/edu/sjsu/fall2016/cs175/fragmentdemo/DetailActivity.java
+++ FragmentDemo/app/src/main/java/edu/sjsu/fall2016/cs175/fragmentdemo/DetailActivity.java
@@ -1,7 +1,12 @@
 package edu.sjsu.fall2016.cs175.fragmentdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -9,5 +14,28 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        Intent intent = getIntent();
+        String pokemonName = intent.getStringExtra("pokemon_name");
+
+        ImageView image = (ImageView) findViewById(R.id.pokemon);
+        int imageId = getResources().getIdentifier(pokemonName.toLowerCase(),"drawable",getPackageName());
+        image.setImageResource(imageId);
+
+        TextView textView = (TextView) findViewById(R.id.name);
+        textView.setText(pokemonName);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
--- FragmentDemo/app/src/main/java/edu/sjsu/fall2016/cs175/fragmentdemo/Pokedex.java
+++ FragmentDemo/app/src/main/java/edu/sjsu/fall2016/cs175/fragmentdemo/Pokedex.java
@@ -1,8 +1,13 @@
 package edu.sjsu.fall2016.cs175.fragmentdemo;
 
+import android.app.Fragment;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class Pokedex extends AppCompatActivity {
@@ -14,7 +19,20 @@ public class Pokedex extends AppCompatActivity {
     }
 
     public void showDetail(View view) {
-        Intent intent = new Intent(this, DetailActivity.class);
-        startActivity(intent);
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra("pokemon_name", view.getTag().toString());
+            startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
